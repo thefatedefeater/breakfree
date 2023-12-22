52f818cbcf831c7f538c63ba894f784278b33b59
demarcush <146051763+demarcush@users.noreply.github.com>
2023-12-22 21:17:46 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,7 +10,7 @@ curl -fsS http://localhost:8080/servers > servers.json
 curl -fsS http://localhost:8080/servers-lite > servers-lite.json
 
 #quick-fix
-sed s/\;mux\=true//g servers.json servers-lite.json
+sed -i s/\;mux\=true//g servers.json servers-lite.json
 
 python main.py servers.json template-ir.json ir.json
 python main.py servers.json template-ir-sfw.json ir-sfw.json
