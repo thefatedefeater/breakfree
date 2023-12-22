c6b894173c0520618eecc61b095d9777aea9f2f6
demarcush <146051763+demarcush@users.noreply.github.com>
2023-12-22 21:13:22 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,26 +3,29 @@ export GOPROXY="https://proxy.golang.org,direct"
 
 git clone https://github.com/SagerNet/serenity
 cd serenity && make install && cd ..
-#sleep 60
 serenity run &
 sleep 20
 
 curl -fsS http://localhost:8080/servers > servers.json
 curl -fsS http://localhost:8080/servers-lite > servers-lite.json
 
+#quick-fix
+sed s/\;mux\=true//g servers.json servers-lite.json
+
 python main.py servers.json template-ir.json ir.json
 python main.py servers.json template-ir-sfw.json ir-sfw.json
 python main.py servers-lite.json template-ir.json ir-lite.json
 python main.py servers-lite.json template-ir-sfw.json ir-sfw-lite.json
+echo "IR files exported!"
 
 python main.py servers.json template-cn.json cn.json
 python main.py servers.json template-cn-sfw.json cn-sfw.json
 python main.py servers-lite.json template-cn.json cn-lite.json
 python main.py servers-lite.json template-cn-sfw.json cn-sfw-lite.json
+echo "CN files exported!"
 
 git clone https://github.com/SagerNet/sing-box
-cd sing-box && make install && cd ..
-#sleep 60
+cd sing-box && git checkout main && make install && cd ..
 
 for i in ir*.json cn*.json; do sing-box -c "$i" check && echo "'$i' is OK!"; done
 echo "SUCCESS!"
