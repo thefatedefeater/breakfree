cf86187d37cd7812f79626b831017434b8f16381
demarcush <146051763+demarcush@users.noreply.github.com>
2024-04-28 23:31:18 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 export GOPROXY="https://proxy.golang.org,direct"
 
 git clone https://github.com/SagerNet/serenity
-cd serenity && make install && cd ..
+cd serenity && git checkout main && make install && cd ..
 serenity -c serenity.json run &
 sleep 20
 
